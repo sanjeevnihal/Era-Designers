e880a0a7da2102caa6e008111f5e5753bb09d9ee
sanjeevnihal <39952890+sanjeevnihal@users.noreply.github.com>
2021-12-15 16:31:13 +0530

    -Polished Home Page -Most of the CSS for the home page is finished -Carousel is Working -Carousel items css need to be modified

---
+++ index.js
@@ -0,0 +1,31 @@
+
+
+$(".top-container").owlCarousel({
+
+margin: 0,
+loop: true,
+// autoplay: true,
+// autoplayTimeout: 2000,
+// autoplayHoverPause: true,
+
+responsive: {
+
+  0: {
+    items: 1,
+    nav: false
+  },
+
+  600: {
+    items: 1,
+    nav: false
+  },
+
+  1000: {
+    items: 1,
+    nav: false
+  }
+
+
+}
+
+});
